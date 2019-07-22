520f075147247e112d545d472b6e7aff4ee8bdf3
maonaparede <jefferson9312@gmail.com>
2019-07-22 13:19:29 -0300

    Trabalho POO. a0.4

--- app/src/main/java/com/example/trabalho1/Tela3.java
+++ app/src/main/java/com/example/trabalho1/Tela3.java
@@ -2,12 +2,38 @@ package com.example.trabalho1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
 
 public class Tela3 extends AppCompatActivity {
+
+
+    CheckBox azul = (CheckBox) findViewById(R.id.azul);
+    CheckBox verde = (CheckBox) findViewById(R.id.verde);
+    CheckBox vermelho = (CheckBox) findViewById(R.id.vermelho);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela3);
+
+
+
+        }
+
+        public void proxima(View view){
+            String texto = "você selecionou:";
+
+            if (azul.isChecked()) {
+                texto += "Azul ";
+            }
+            if (verde.isChecked()) {
+                texto += "Verde ";
+            }
+            if (vermelho.isChecked()) {
+                texto += "Vermelho";
+            }
+        }
     }
-}
